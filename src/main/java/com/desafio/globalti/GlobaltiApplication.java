74facac14ae0d2e6d20af18ae38300f75e2d1069
matheus-prado <SerenoPrado@gmail.com>
2025-09-30 14:11:27 -0300

    Primeiro commit

---
+++ src/main/java/com/desafio/globalti/GlobaltiApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.globalti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltiApplication.class, args);
+	}
+
+}
